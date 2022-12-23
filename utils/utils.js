e7195250738bdc8b82f9a87f83d71368b32e6f7a
JDRjuju <94639280+JDRjuju@users.noreply.github.com>
2022-12-23 19:59:24 +0100

    correction utils

--- utils/utils.js
+++ utils/utils.js
@@ -1,7 +1,7 @@
 module.exports.checkUniqueName = (uniqueName) => {
   // regex to only accept "., "-" and " "
 
-  const regex = /[^\w .-]/g;
+  const regex = /[^\w .-]|_/g;
   const found = uniqueName.match(regex);
   console.log(found);
   if (found === null || found.length === 0) return true;
