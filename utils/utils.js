ac6b7af7ffc6a53b7922a8b656d09253200f4f95
Brulans4 <118511976+Brulans4@users.noreply.github.com>
2022-12-23 21:35:17 +0100

    Update utils.js

--- utils/utils.js
+++ utils/utils.js
@@ -1,7 +1,7 @@
 module.exports.checkUniqueName = (uniqueName) => {
   // regex to only accept "., "-", " " and caps
 
-  const regex = /[^A-Z. -]|_/g;
+  const regex = /[^A-Z.-]|_/g;
   const found = uniqueName.match(regex);
   console.log(found);
   if (found === null || found.length === 0) return true;
